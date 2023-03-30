e809dc4f647fb6f2af80ee65f4191cfc874299f8
ismaelnobregadev <109975003+ismaelnobregadev@users.noreply.github.com>
2023-03-29 23:08:23 -0300

    Update config.js

--- charts/recharts/pie/src/config.js
+++ charts/recharts/pie/src/config.js
@@ -1,8 +1,8 @@
 export const qlikConfig = {
-    host: "qap.sebrae.com.br/usina", // Qlik Sense Host
+    host: "qap.sebrae.com.br", // Qlik Sense Host
     secure: true, // Whether your host is secure of not (HTTPS / HTTP)
     port: 443, // Qlik Sense site port
-    prefix: "", // Prefix
+    prefix: "usina", // Prefix
     appId: "d183a25e-de1d-47a0-b2a0-459dfbbc20d1", // Application Id
     qcs: false, // whether you are connecting to a Qlik Cloud site or not
   
